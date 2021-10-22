849c7ce6e5b131625eb9ac51519057dd83f6260e
Felix Seifert <mail@felix-seifert.com>
2021-10-22 09:29:50 +0200

    Create new df with statistical values
    * Transform values to [ms] before statistical calculations
    * Separate reading of .csv and calculation of statistical values into separate DFs

--- process-benchmarks/generate_graphs.py
+++ process-benchmarks/generate_graphs.py
@@ -6,19 +6,28 @@ from pandas import DataFrame
 def read_list_processing_df() -> DataFrame:
     df = pd.read_csv('../benchmark.csv', header=0)
     df = df.drop(['Description'], axis=1)
-    df['median_ns'] = df.median(axis=1, numeric_only=True)
-    df['median_ms'] = df['median_ns'] / 1000000
+
+    # Conversion from [ns] to [ms]
+    df.iloc[:, 2:] = df.iloc[:, 2:] / 1000000
 
     return df
+
+
+def calculate_statistics_df(df: DataFrame):
+    statistics = df[['Processing', 'Mode']].copy()
+    statistics['mean'] = df.mean(axis=1, numeric_only=True)
+    statistics['median'] = df.median(axis=1, numeric_only=True)
+    statistics['median'] = df.median(axis=1, numeric_only=True)
+    statistics['std'] = df.std(axis=1, numeric_only=True)
+    return statistics
 
 
 def get_comparison_df(df: DataFrame):
     list_processing = df.copy().iloc[0:4]
     no_processing = df.copy().iloc[4:6]
-    no_processing_ext = no_processing.append(no_processing)
+    no_processing = no_processing.append(no_processing)
 
-    list_processing['median_difference_ns'] = list_processing['median_ns'] - no_processing_ext['median_ns'].values
-    list_processing['median_difference_ms'] = list_processing['median_difference_ns'] / 1000000
+    list_processing['median_difference'] = list_processing['median'] - no_processing['median'].values
 
     return list_processing
 
@@ -76,15 +85,16 @@ def create_absolute_graph(df_to_display: DataFrame, no_of_execs: int) -> Figure:
 def create_comparison_graph(df_to_display: DataFrame, no_of_execs: int) -> Figure:
     ax = create_formatted_graph(df_to_display)
     ax.set_xlabel('List processing form')
-    ax.set_ylabel("Median duration over {0} executions \n compared to no list processing [ms]".format(no_of_execs))
+    ax.set_ylabel("Median duration difference over {0} executions \n when compared to no list processing [ms]".format(no_of_execs))
     return ax.get_figure()
 
 
 list_processing_df = read_list_processing_df()
-comparison_df = get_comparison_df(list_processing_df)
+statistics_df = calculate_statistics_df(list_processing_df)
+comparison_df = get_comparison_df(statistics_df)
 
-absolute_pivot_table = get_pivot_table(list_processing_df, 'median_ms')
-comparison_pivot_table = get_pivot_table(comparison_df, 'median_difference_ms')
+absolute_pivot_table = get_pivot_table(statistics_df, 'median')
+comparison_pivot_table = get_pivot_table(comparison_df, 'median_difference')
 
 execution_columns = list(filter(lambda c: c.startswith('T'), list_processing_df.columns))
 no_of_executions = len(execution_columns)
